ecc3da42ac8a428fa47a482d2464fe7ecafa42d0
amber-reichert <amber.lautigar@gmail.com>
2013-04-09 15:07:43 -0400

    Dates formatted, date precision field filled in for "Year" and "Month" entries
    Dates have been formatted in YYYY/MM/DD format. Date precision field is
    filled in for exception cases ("Year" and "Month"). Refined a few "Fall
    YYYY" etc cases.

--- files/lbjohnson.php
+++ files/lbjohnson.php
@@ -1,62 +1,62 @@
 
-<h2>11 22, 1963</h2>"","",<p>Lyndon Baines Johnson is sworn in as the thirty-sixth President of the United States following the assassination of John F. Kennedy.</p>
-<h2>11 27, 1963</h2>"","",<p>Johnson addresses a joint session of Congress calling on legislators to fulfill Kennedy's legacy and pass civil rights and tax legislation. </p>
-<h2>11 29, 1963</h2>"","",<p>Johnson creates a special commission chaired by Chief Justice Earl Warren to investigate the Kennedy assassination. </p>
-<h2>1 23, 1964</h2>"","",<p>The Twenty-Fourth Amendment to the Constitution is ratified, abolishing poll taxes. </p>
-<h2>2 7, 1964</h2>"","",<p>The Beatles arrive in New York for their first U.S. tour. </p>
-<h2>3 14, 1964</h2>"","",<p>Jack Ruby is convicted of the murder of Lee Harvey Oswald and sentenced to death. </p>
-<h2>5 22, 1964</h2>"","",<p>In a speech at the University of Michigan, Johnson announces his intention to create a Great Society by extending American prosperity to all its citizens. </p>
-<h2>7 2, 1964</h2>"","",<p>Johnson signs The Civil Rights Act of 1964. </p>
-<h2>7 15, 1964</h2>"","",<p>Sen. Barry Goldwater (R-AZ) receives the Republican nomination for President. </p>
-<h2>7 19, 1964</h2>"","",<p>Governor George Wallace of Alabama drops out of the presidential race despite strong showings in several Democratic primaries. </p>
-<h2>8 4, 1964</h2>"","",<p>Three civil rights workers are found dead in Mississippi. James Chaney, Andrew Goodman, and Michael Schwerner were all participating in the Mississippi Freedom Summer. </p>
-<h2>8 7, 1964</h2>"","",<p>Congress passes the Gulf of Tonkin Resolution giving the President power to pursue military action in Vietnam. </p>
-<h2>8 26, 1964</h2>"","",<p>Johnson receives the Democratic nomination for President. Senator Hubert H. Humphrey (D-MN) is nominated as the vice-presidential candidate. </p>
-<h2>8 30, 1964</h2>"","",<p>Johnson signs the Economic Opportunity Act, creating the Office of Economic Opportunity and beginning the War on Poverty. </p>
-<h2>9 27, 1964</h2>"","",<p>The Warren Commission releases its report, rejecting the notion that Kennedy was assassinated as part of a conspiracy. </p>
-<h2>10 14, 1964</h2>"","",<p>Martin Luther King Jr. is awarded the Nobel Peace Prize. </p>
-<h2>10 15, 1964</h2>"","",<p>Nikita Khrushchev is forced to resign as leader of the Soviet Union and is replaced by Leonid Brezhnev. </p>
-<h2>11 3, 1964</h2>"","",<p>Lyndon B. Johnson is elected President of the United States. </p>
-<h2>1 20, 1965</h2>"","",<p>Johnson is inaugurated President of the United States. </p>
-<h2>2 9, 1965</h2>"","",<p>Nine American soldiers are killed in an attack on U.S. barracks in Pleiku, Vietnam. Johnson begins the bombing of North Vietnam. </p>
-<h2>2 21, 1965</h2>"","",<p>Malcolm X is assassinated by other black Muslims in New York City. </p>
-<h2>3 15, 1965</h2>"","",<p>Johnson calls for voting rights legislation. </p>
-<h2>3 21, 1965</h2>"","",<p>Martin Luther King Jr. leads a march from Selma to Montgomery, Alabama. </p>
-<h2>4 11, 1965</h2>"","",<p>Johnson signs the Elementary and Secondary Education Act. </p>
-<h2>4 28, 1965</h2>"","",<p>Johnson sends U.S. marines to the Dominican Republic to protect U.S. citizens after a military coup. </p>
-<h2>6 7, 1965</h2>"","",<p>The U.S. Supreme Court finds a Connecticut law banning the use of contraceptives unconstitutional. </p>
-<h2>7 26, 1965</h2>"","",<p>Martin Luther King Jr. leads a demonstration in Chicago in an attempt to bring the Civil Rights Movement to the North. </p>
-<h2>7 28, 1965</h2>"","",<p>Johnson increases the number of troops sent to Vietnam, indicating his determination to engage in a ground war. </p>
-<h2>7 30, 1965</h2>"","",<p>Johnson signs legislation creating Medicare and Medicaid. </p>
-<h2>8 , 1965</h2>"","",<p>A paper by Daniel Patrick Moynihan, entitled, <i>The Negro Family: The Case For National Action</i>, is released. The conclusions of the "Moynihan Report" create heated controversy. </p>
-<h2>8 5, 1965</h2>"","",<p>Johnson signs the Voting Rights Act into law. </p>
-<h2>8 11, 1965</h2>"8 16, 1965","",<p>The Watts Riots break out in Los Angeles. </p>
-<h2>2 21, 1966</h2>"","",<p>Fearing that American involvement in Vietnam will draw France into a world war, French president Charles de Gaulle announces that France will withdraw from NATO. </p>
-<h2>3 7, 1966</h2>"","",<p>The U.S. Supreme Court unanimously upholds the Voting Rights Act of 1965. </p>
-<h2>6 1, 1966</h2>"6 2, 1966","",<p>The White House Conference on Civil Rights urges Congress to pass further civil rights legislation. </p>
-<h2>6 6, 1966</h2>"","",<p>James Meredith is shot in a March from Memphis, Tennessee, to Jackson, Mississippi; civil rights leaders organize to complete his march. During this demonstration, Stokely Carmichael makes a statement in support of "black power." </p>
-<h2>6 13, 1966</h2>"","",<p>In <i>Miranda v. Arizona</i>, the U.S. Supreme Court rules that the constitutional provision against self-incrimination applies to police interrogations. </p>
-<h2>10 , 1966</h2>"","",<p>Huey P. Newton and Bobby Seale found the Black Panther Party in Oakland, California. </p>
-<h2>1 27, 1967</h2>"","",<p>A launch pad fire during tests for the Apollo program kills three astronauts. </p>
-<h2>2 10, 1967</h2>"","",<p>The Twenty-Fifth Amendment to the Constitution is ratified, providing rules of succession upon the death or incapacitation of the President, and enabling the President to appoint a new vice-president in the case of a vacancy. </p>
-<h2>6 5, 1967</h2>"","",<p>The Six Day War breaks out between Israel and several Arab nations. </p>
-<h2>6 14, 1967</h2>"","",<p>Johnson appoints Thurgood Marshall to the Supreme Court. </p>
-<h2>7 13, 1967</h2>"","",<p>Riots break out in Newark, New Jersey.</p>
-<h2>7 23, 1967</h2>"","",<p>Riots spread across the city of Detroit, Michigan. </p>
-<h2>10 21, 1967</h2>"","",<p>Anti-war demonstrators March to the Pentagon in an attempt to shut it down. </p>
-<h2>1 22, 1968</h2>"","",<p>North Korean forces capture the <i>U.S.S. Pueblo</i>. </p>
-<h2>1 30, 1968</h2>"","",<p>North Vietnamese troops surprise South Vietnamese and American troops by attacking during the Tet holiday. While the Tet Offensive is not a military loss for the United States, it leads to a loss of confidence in the Johnson administration's prosecution of the war. </p>
-<h2>2 8, 1968</h2>"","",<p>George Wallace enters the presidential race as an independent. </p>
-<h2>3 12, 1968</h2>"","",<p>Johnson wins the New Hampshire Democratic primary, but anti-war candidate Eugene McCarthy comes in a close second with 42 percent of the vote. </p>
-<h2>3 16, 1968</h2>"","",<p>U.S. forces in Vietnam commit massacre in the hamlet of My Lai; hundreds of unarmed men, women, and children are killed. News of the event would not reach the public until November 1969. </p>
-<h2>3 16, 1968</h2>"","",<p>Robert Kennedy enters the race for the Democratic nomination for President. </p>
-<h2>3 31, 1968</h2>"","",<p>Johnson announces a partial bombing halt and his unwillingness to seek reelection to the presidency. </p>
-<h2>4 4, 1968</h2>"","",<p>Martin Luther King Jr. is assassinated in Memphis, Tennessee. </p>
-<h2>4 23, 1968</h2>"","",<p>Students at Columbia University take over several buildings on campus. </p>
-<h2>4 29, 1968</h2>"","",<p>Ralph Abernathy of the Southern Christian Leadership Conference (SCLC) begins the Poor People's Campaign in Washington, D.C.</p>
-<h2>5 13, 1968</h2>"","",<p>The United States and North Vietnam begin peace talks in Paris. </p>
-<h2>6 5, 1968</h2>"","",<p>Senator Robert Kennedy is assassinated after winning the Democratic primary in California. </p>
-<h2>8 20, 1968</h2>"8 21, 1968","",<p>The Soviet Union invades Czechoslovakia to end the movement toward greater freedom and independence. </p>
-<h2>8 25, 1968</h2>"8 29, 1968","",<p>Hubert H. Humphrey is nominated in Chicago as the Democratic candidate for President.  Demonstrators and police clash in violent confrontations. </p>
-<h2>11 5, 1968</h2>"","",<p>Richard M. Nixon is elected President of the United States, and Spiro Agnew is elected vice-president. </p>
-<h2>11 12, 1968</h2>"","",<p>Leonid Brezhnev announces that the Soviet Union has the right to intervene anywhere in its sphere of influence. This "Brezhnev Doctrine" becomes central to Soviet foreign policy. </p>
+<h2>1963/11/22</h2>"","",<p>Lyndon Baines Johnson is sworn in as the thirty-sixth President of the United States following the assassination of John F. Kennedy.</p>
+<h2>1963/11/27</h2>"","",<p>Johnson addresses a joint session of Congress calling on legislators to fulfill Kennedy's legacy and pass civil rights and tax legislation. </p>
+<h2>1963/11/29</h2>"","",<p>Johnson creates a special commission chaired by Chief Justice Earl Warren to investigate the Kennedy assassination. </p>
+<h2>1964/1/23</h2>"","",<p>The Twenty-Fourth Amendment to the Constitution is ratified, abolishing poll taxes. </p>
+<h2>1964/2/7</h2>"","",<p>The Beatles arrive in New York for their first U.S. tour. </p>
+<h2>1964/3/14</h2>"","",<p>Jack Ruby is convicted of the murder of Lee Harvey Oswald and sentenced to death. </p>
+<h2>1964/5/22</h2>"","",<p>In a speech at the University of Michigan, Johnson announces his intention to create a Great Society by extending American prosperity to all its citizens. </p>
+<h2>1964/7/2</h2>"","",<p>Johnson signs The Civil Rights Act of 1964. </p>
+<h2>1964/7/15</h2>"","",<p>Sen. Barry Goldwater (R-AZ) receives the Republican nomination for President. </p>
+<h2>1964/7/19</h2>"","",<p>Governor George Wallace of Alabama drops out of the presidential race despite strong showings in several Democratic primaries. </p>
+<h2>1964/8/4</h2>"","",<p>Three civil rights workers are found dead in Mississippi. James Chaney, Andrew Goodman, and Michael Schwerner were all participating in the Mississippi Freedom Summer. </p>
+<h2>1964/8/7</h2>"","",<p>Congress passes the Gulf of Tonkin Resolution giving the President power to pursue military action in Vietnam. </p>
+<h2>1964/8/26</h2>"","",<p>Johnson receives the Democratic nomination for President. Senator Hubert H. Humphrey (D-MN) is nominated as the vice-presidential candidate. </p>
+<h2>1964/8/30</h2>"","",<p>Johnson signs the Economic Opportunity Act, creating the Office of Economic Opportunity and beginning the War on Poverty. </p>
+<h2>1964/9/27</h2>"","",<p>The Warren Commission releases its report, rejecting the notion that Kennedy was assassinated as part of a conspiracy. </p>
+<h2>1964/10/14</h2>"","",<p>Martin Luther King Jr. is awarded the Nobel Peace Prize. </p>
+<h2>1964/10/15</h2>"","",<p>Nikita Khrushchev is forced to resign as leader of the Soviet Union and is replaced by Leonid Brezhnev. </p>
+<h2>1964/11/3</h2>"","",<p>Lyndon B. Johnson is elected President of the United States. </p>
+<h2>1965/1/20</h2>"","",<p>Johnson is inaugurated President of the United States. </p>
+<h2>1965/2/9</h2>"","",<p>Nine American soldiers are killed in an attack on U.S. barracks in Pleiku, Vietnam. Johnson begins the bombing of North Vietnam. </p>
+<h2>1965/2/21</h2>"","",<p>Malcolm X is assassinated by other black Muslims in New York City. </p>
+<h2>1965/3/15</h2>"","",<p>Johnson calls for voting rights legislation. </p>
+<h2>1965/3/21</h2>"","",<p>Martin Luther King Jr. leads a march from Selma to Montgomery, Alabama. </p>
+<h2>1965/4/11</h2>"","",<p>Johnson signs the Elementary and Secondary Education Act. </p>
+<h2>1965/4/28</h2>"","",<p>Johnson sends U.S. marines to the Dominican Republic to protect U.S. citizens after a military coup. </p>
+<h2>1965/6/7</h2>"","",<p>The U.S. Supreme Court finds a Connecticut law banning the use of contraceptives unconstitutional. </p>
+<h2>1965/7/26</h2>"","",<p>Martin Luther King Jr. leads a demonstration in Chicago in an attempt to bring the Civil Rights Movement to the North. </p>
+<h2>1965/7/28</h2>"","",<p>Johnson increases the number of troops sent to Vietnam, indicating his determination to engage in a ground war. </p>
+<h2>1965/7/30</h2>"","",<p>Johnson signs legislation creating Medicare and Medicaid. </p>
+<h2>1965/8/1</h2>"","Month",<p>A paper by Daniel Patrick Moynihan, entitled, <i>The Negro Family: The Case For National Action</i>, is released. The conclusions of the "Moynihan Report" create heated controversy. </p>
+<h2>1965/8/5</h2>"","",<p>Johnson signs the Voting Rights Act into law. </p>
+<h2>1965/8/11</h2>"1965/8/16","",<p>The Watts Riots break out in Los Angeles. </p>
+<h2>1966/2/21</h2>"","",<p>Fearing that American involvement in Vietnam will draw France into a world war, French president Charles de Gaulle announces that France will withdraw from NATO. </p>
+<h2>1966/3/7</h2>"","",<p>The U.S. Supreme Court unanimously upholds the Voting Rights Act of 1965. </p>
+<h2>1966/6/1</h2>"1966/6/2","",<p>The White House Conference on Civil Rights urges Congress to pass further civil rights legislation. </p>
+<h2>1966/6/6</h2>"","",<p>James Meredith is shot in a March from Memphis, Tennessee, to Jackson, Mississippi; civil rights leaders organize to complete his march. During this demonstration, Stokely Carmichael makes a statement in support of "black power." </p>
+<h2>1966/6/13</h2>"","",<p>In <i>Miranda v. Arizona</i>, the U.S. Supreme Court rules that the constitutional provision against self-incrimination applies to police interrogations. </p>
+<h2>1966/10/1</h2>"","Month",<p>Huey P. Newton and Bobby Seale found the Black Panther Party in Oakland, California. </p>
+<h2>1967/1/27</h2>"","",<p>A launch pad fire during tests for the Apollo program kills three astronauts. </p>
+<h2>1967/2/10</h2>"","",<p>The Twenty-Fifth Amendment to the Constitution is ratified, providing rules of succession upon the death or incapacitation of the President, and enabling the President to appoint a new vice-president in the case of a vacancy. </p>
+<h2>1967/6/5</h2>"","",<p>The Six Day War breaks out between Israel and several Arab nations. </p>
+<h2>1967/6/14</h2>"","",<p>Johnson appoints Thurgood Marshall to the Supreme Court. </p>
+<h2>1967/7/13</h2>"","",<p>Riots break out in Newark, New Jersey.</p>
+<h2>1967/7/23</h2>"","",<p>Riots spread across the city of Detroit, Michigan. </p>
+<h2>1967/10/21</h2>"","",<p>Anti-war demonstrators March to the Pentagon in an attempt to shut it down. </p>
+<h2>1968/1/22</h2>"","",<p>North Korean forces capture the <i>U.S.S. Pueblo</i>. </p>
+<h2>1968/1/30</h2>"","",<p>North Vietnamese troops surprise South Vietnamese and American troops by attacking during the Tet holiday. While the Tet Offensive is not a military loss for the United States, it leads to a loss of confidence in the Johnson administration's prosecution of the war. </p>
+<h2>1968/2/8</h2>"","",<p>George Wallace enters the presidential race as an independent. </p>
+<h2>1968/3/12</h2>"","",<p>Johnson wins the New Hampshire Democratic primary, but anti-war candidate Eugene McCarthy comes in a close second with 42 percent of the vote. </p>
+<h2>1968/3/16</h2>"","",<p>U.S. forces in Vietnam commit massacre in the hamlet of My Lai; hundreds of unarmed men, women, and children are killed. News of the event would not reach the public until November 1969. </p>
+<h2>1968/3/16</h2>"","",<p>Robert Kennedy enters the race for the Democratic nomination for President. </p>
+<h2>1968/3/31</h2>"","",<p>Johnson announces a partial bombing halt and his unwillingness to seek reelection to the presidency. </p>
+<h2>1968/4/4</h2>"","",<p>Martin Luther King Jr. is assassinated in Memphis, Tennessee. </p>
+<h2>1968/4/23</h2>"","",<p>Students at Columbia University take over several buildings on campus. </p>
+<h2>1968/4/29</h2>"","",<p>Ralph Abernathy of the Southern Christian Leadership Conference (SCLC) begins the Poor People's Campaign in Washington, D.C.</p>
+<h2>1968/5/13</h2>"","",<p>The United States and North Vietnam begin peace talks in Paris. </p>
+<h2>1968/6/5</h2>"","",<p>Senator Robert Kennedy is assassinated after winning the Democratic primary in California. </p>
+<h2>1968/8/20</h2>"1968/8/21","",<p>The Soviet Union invades Czechoslovakia to end the movement toward greater freedom and independence. </p>
+<h2>1968/8/25</h2>"1968/8/29","",<p>Hubert H. Humphrey is nominated in Chicago as the Democratic candidate for President.  Demonstrators and police clash in violent confrontations. </p>
+<h2>1968/11/5</h2>"","",<p>Richard M. Nixon is elected President of the United States, and Spiro Agnew is elected vice-president. </p>
+<h2>1968/11/12</h2>"","",<p>Leonid Brezhnev announces that the Soviet Union has the right to intervene anywhere in its sphere of influence. This "Brezhnev Doctrine" becomes central to Soviet foreign policy. </p>
--- files/polk.php
+++ files/polk.php
@@ -1,28 +1,28 @@
 
-<h2>3 4, 1845</h2>"","",<p>James K. Polk is inaugurated as the eleventh President of the United States.</p>
-<h2>6 25, 1845</h2>"","",<p>General Zachary Taylor receives orders from Polk to move his troops from Fort Jesup in Louisiana to a position "on or near the Rio Grande" in Texas to discourage a Mexican invasion.</p>
-<h2>7 1845</h2>"","",<p>New York Jacksonian Democrat, John L. O'Sullivan, accuses opponents of Texas annexation of "limiting our greatness and checking the fulfillment of our manifest destiny to overspread the continent allotted by Providence for the free development of our yearly multiplying millions."  The term, "Manifest Destiny," quickly becomes the label used to encapsulate the belief among Americans in their country's God-given right to expand its territory and institutions.</p>
-<h2>10 10, 1845</h2>"","",<p>Under the direction of Secretary of the Navy George Bancroft, the Naval Academy opens at Annapolis, Maryland.</p>
-<h2>12 29, 1845</h2>"","",<p>Texas is admitted as a slave state, making it the twenty-eighth state in the Union.</p>
-<h2>1845</h2>"1846","",<p>The great Irish potato famine forces huge waves of starving immigrants to the United States, sparking anti-Catholic, nativist backlashes.</p>
-<h2>2 4, 1846</h2>"","",<p>The Mormon migration to Utah, led by Brigham Young, begins. </p>
-<h2>5 13, 1846</h2>"","",<p>Congress declares war on Mexico after American troops, under General Zachary Taylor, clash with Mexican troops on the north bank of the Rio Grande. </p>
-<h2>6 14, 1846</h2>"","",<p>In the Bear Flag Revolt, approximately thirty American settlers (anticipating the Mexican War) take over a small Mexican garrison in Sonoma, California, and declare California a free and independent republic. </p>
-<h2>6 15, 1846</h2>"","",<p>The Oregon Treaty establishes the 49th parallel as the border between British and American claims to the Oregon Territory, granting the United States clear title to present-day Idaho, Washington, Oregon, and Montana, while granting to Britain territory above the 49th parallel and full control over Vancouver Island. </p>
-<h2>7 29, 1846</h2>"","",<p>Congress passes the Tariff of 1846, a key part of President's Polk's domestic agenda. Known as the "Walker Tariff," after Polk's secretary of the Treasury, Robert J. Walker, the Tariff of 1846 lowers rates toward revenue-only levels, although a few items remain protected.</p>
-<h2>8 3, 1846</h2>"","",<p>President Polk vetoes a river and harbors bill which would have provided for federally funded internal improvements. Like Andrew Jackson in his veto of the Maysville Road bill, Polk argues that the bill unfairly favors particular areas, including ports which have no foreign trade. Therefore, says Polk, the bill is unconstitutional.</p>
-<h2>8 6, 1846</h2>"","",<p>President Polk signs into law the Independent Treasury, which he calls a "Constitutional Treasury," an integral part of his domestic agenda.</p>
-<h2>8 10, 1846</h2>"","",<p>Congress establishes the Smithsonian Institution. </p>
-<h2>11 1846</h2>"","",<p>The Whigs regain a majority of seats in the House of Representatives. </p>
-<h2>12 28, 1846</h2>"","",<p>Iowa is admitted as a free state, making it the twenty-ninth state in the Union.</p>
-<h2>2 22, 1847</h2>"2 23, 1847","",<p>General Zachary Taylor defeats the Mexicans under General Santa Anna at the Battle of Buena Vista. Taylor's victory cements his growing acclaim as a national hero and helps propel him to the 1848 Whig nomination for President. </p>
-<h2>7 22, 1847</h2>"","",<p>The first Mormon settlers arrive in the Salt Lake Valley. </p>
-<h2>9 14, 1847</h2>"","",<p> General Winfield Scott takes Mexico City, adding pressure to the Mexican government to submit to the demands of President Polk and sign a treaty of peace. </p>
-<h2>1 24, 1848</h2>"","",<p> James Marshall discovers gold near Sacramento, California. The discovery begins the massive migrations of the California gold rush, allowing the territory to become a state and setting off fierce debates over whether to admit California as a free or slave state. </p>
-<h2>2 2, 1848</h2>"","",<p>The Treaty of Guadalupe Hidalgo ends the Mexican War and grants the United States vast territories, including all or large parts of present-day California, Arizona, New Mexico, Colorado, Nevada, Utah, and Wyoming, and confirms the incorporation of Texas as part of the United States. The acquisition of these lands aggravates growing sectionalism in the country over the future of slavery in the Union. </p>
-<h2>5 29, 1848</h2>"","",<p>Wisconsin is admitted as a free state, making it the thirtieth state in the Union. </p>
-<h2>7 12, 1848</h2>"7 20, 1848","",<p>Elizabeth Cady Stanton and Lucretia Mott hold a women's rights convention in Seneca Falls, New York. Among other demands, the convention&rsquo;s "Declaration of Sentiments" calls for women's suffrage.</p>
-<h2>11 7, 1848</h2>"","",<p>Zachary Taylor is elected the twelfth President of the United States. </p>
-<h2>11 1848</h2>"","",<p>Democrats regain a majority of seats in the House of Representatives. </p>
-<h2>3 3, 1849</h2>"","",<p>The United States Department of Interior is created. It combines the General Land Office, the Bureau of Indian Affairs, the Pension Office, the Bureau of the Census, and the Patent Office into a single department. </p>
-<h2>3 5, 1849</h2>"","",<p>Zachary Taylor is inaugurated as the twelfth President of the United States, and Polk begins a goodwill tour of the South that will eventually end in Tennessee.</p>
+<h2>1845/3/4</h2>"","",<p>James K. Polk is inaugurated as the eleventh President of the United States.</p>
+<h2>1845/6/25</h2>"","",<p>General Zachary Taylor receives orders from Polk to move his troops from Fort Jesup in Louisiana to a position "on or near the Rio Grande" in Texas to discourage a Mexican invasion.</p>
+<h2>1845/7/1</h2>"","Month",<p>New York Jacksonian Democrat, John L. O'Sullivan, accuses opponents of Texas annexation of "limiting our greatness and checking the fulfillment of our manifest destiny to overspread the continent allotted by Providence for the free development of our yearly multiplying millions."  The term, "Manifest Destiny," quickly becomes the label used to encapsulate the belief among Americans in their country's God-given right to expand its territory and institutions.</p>
+<h2>1845/10/10</h2>"","",<p>Under the direction of Secretary of the Navy George Bancroft, the Naval Academy opens at Annapolis, Maryland.</p>
+<h2>1845/12/29</h2>"","",<p>Texas is admitted as a slave state, making it the twenty-eighth state in the Union.</p>
+<h2>1845/1/1</h2>"1846/12/31","Year",<p>The great Irish potato famine forces huge waves of starving immigrants to the United States, sparking anti-Catholic, nativist backlashes.</p>
+<h2>1846/2/4</h2>"","",<p>The Mormon migration to Utah, led by Brigham Young, begins. </p>
+<h2>1846/5/13</h2>"","",<p>Congress declares war on Mexico after American troops, under General Zachary Taylor, clash with Mexican troops on the north bank of the Rio Grande. </p>
+<h2>1846/6/14</h2>"","",<p>In the Bear Flag Revolt, approximately thirty American settlers (anticipating the Mexican War) take over a small Mexican garrison in Sonoma, California, and declare California a free and independent republic. </p>
+<h2>1846/6/15</h2>"","",<p>The Oregon Treaty establishes the 49th parallel as the border between British and American claims to the Oregon Territory, granting the United States clear title to present-day Idaho, Washington, Oregon, and Montana, while granting to Britain territory above the 49th parallel and full control over Vancouver Island. </p>
+<h2>1846/7/29</h2>"","",<p>Congress passes the Tariff of 1846, a key part of President's Polk's domestic agenda. Known as the "Walker Tariff," after Polk's secretary of the Treasury, Robert J. Walker, the Tariff of 1846 lowers rates toward revenue-only levels, although a few items remain protected.</p>
+<h2>1846/8/3</h2>"","",<p>President Polk vetoes a river and harbors bill which would have provided for federally funded internal improvements. Like Andrew Jackson in his veto of the Maysville Road bill, Polk argues that the bill unfairly favors particular areas, including ports which have no foreign trade. Therefore, says Polk, the bill is unconstitutional.</p>
+<h2>1846/8/6</h2>"","",<p>President Polk signs into law the Independent Treasury, which he calls a "Constitutional Treasury," an integral part of his domestic agenda.</p>
+<h2>1846/8/10</h2>"","",<p>Congress establishes the Smithsonian Institution. </p>
+<h2>1846/11/1</h2>"","Month",<p>The Whigs regain a majority of seats in the House of Representatives. </p>
+<h2>1846/12/28</h2>"","",<p>Iowa is admitted as a free state, making it the twenty-ninth state in the Union.</p>
+<h2>1847/2/22</h2>"1847/2/23","",<p>General Zachary Taylor defeats the Mexicans under General Santa Anna at the Battle of Buena Vista. Taylor's victory cements his growing acclaim as a national hero and helps propel him to the 1848 Whig nomination for President. </p>
+<h2>1847/7/22</h2>"","",<p>The first Mormon settlers arrive in the Salt Lake Valley. </p>
+<h2>1847/9/14</h2>"","",<p> General Winfield Scott takes Mexico City, adding pressure to the Mexican government to submit to the demands of President Polk and sign a treaty of peace. </p>
+<h2>1848/1/24</h2>"","",<p> James Marshall discovers gold near Sacramento, California. The discovery begins the massive migrations of the California gold rush, allowing the territory to become a state and setting off fierce debates over whether to admit California as a free or slave state. </p>
+<h2>1848/2/2</h2>"","",<p>The Treaty of Guadalupe Hidalgo ends the Mexican War and grants the United States vast territories, including all or large parts of present-day California, Arizona, New Mexico, Colorado, Nevada, Utah, and Wyoming, and confirms the incorporation of Texas as part of the United States. The acquisition of these lands aggravates growing sectionalism in the country over the future of slavery in the Union. </p>
+<h2>1848/5/29</h2>"","",<p>Wisconsin is admitted as a free state, making it the thirtieth state in the Union. </p>
+<h2>1848/7/12</h2>"1848/7/20","",<p>Elizabeth Cady Stanton and Lucretia Mott hold a women's rights convention in Seneca Falls, New York. Among other demands, the convention&rsquo;s "Declaration of Sentiments" calls for women's suffrage.</p>
+<h2>1848/11/7</h2>"","",<p>Zachary Taylor is elected the twelfth President of the United States. </p>
+<h2>1848/11/1</h2>"","Month",<p>Democrats regain a majority of seats in the House of Representatives. </p>
+<h2>1849/3/3</h2>"","",<p>The United States Department of Interior is created. It combines the General Land Office, the Bureau of Indian Affairs, the Pension Office, the Bureau of the Census, and the Patent Office into a single department. </p>
+<h2>1849/3/5</h2>"","",<p>Zachary Taylor is inaugurated as the twelfth President of the United States, and Polk begins a goodwill tour of the South that will eventually end in Tennessee.</p>

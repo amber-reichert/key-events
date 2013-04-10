5c800db7eca76d93db7fdaadf73726b9ad449522
amber-reichert <amber.lautigar@gmail.com>
2013-04-10 17:23:56 -0400

    Presidency added, quotations fixed, file type changed to csv
    Presidency field added and assigned on each. Files changed from .php to
    .csv. Quotations (ie "Black Friday") fixed (“Black Friday”) within
    columns.

--- files/harrison.php
+++
@@ -1,3 +0,0 @@
-
-"1841/03/04","","Day","William Henry Harrison inaugurated as the ninth President of the United States. "
-"1841/04/04","","Day","Harrison dies of pneumonia only one month after his inauguration, making him the first President to die while in office."
